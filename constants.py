37ac6dd77ee89a3bbf673b335f0600efb644c262
palkeeno <mt765432cellos@yahoo.co.jp>
2024-03-28 02:52:03 +0900

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,9 @@
 from datetime import timezone, timedelta
 
+# messages
 MSG_LETS_MINING = "今日も元気に採掘しましょう！ :gem:"
 MSG_ONCE_MINING = "採掘は完了しています！ 採掘は毎日0時と12時にできるよ :pick:"
 MSG_COUNTRY_ROLE = "どこか一つの国に所属してからまた来てね！"
 
+# timezone
 JST = timezone(timedelta(hours=+9), "JST")
