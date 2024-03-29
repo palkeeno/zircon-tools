ec84fdccb7b263dc1c3e487a10ed458c72616d9d
palkeeno <mt765432cellos@yahoo.co.jp>
2024-03-30 00:39:49 +0900

    ランキングが全員操作者になるバグを修正

--- discordbot.py
+++ discordbot.py
@@ -143,7 +143,8 @@ async def get_rank(interaction: discord.Interaction, args=""):
         result = await model.get_user_rank_role(country['role'])
         res_self = [r for r in result if r[1] == interaction.user.id]
         for index, item in enumerate(result):
-            result[index][1] = interaction.user.mention # [0]=rank, [1]=user_mention, [2]=zirnum
+            user = interaction.guild.get_member(item[1])
+            result[index][1] = user.mention # [0]=rank, [1]=user_mention, [2]=zirnum
         embed = make_embed.rank_role(result, res_self[0], country['name'], interaction.user)
         await interaction.response.send_message(embed=embed, ephemeral=True)
     elif args == "country_all":
