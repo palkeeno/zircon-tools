2600f4e300a299b33ced84fe9c72da39463dbb20
palkeeno <mt765432cellos@yahoo.co.jp>
2024-06-25 18:06:45 +0900

    フォーマッタを一部適用

--- config.py
+++ config.py
@@ -1,42 +1,44 @@
 import os
+
 from dotenv import load_dotenv
+
 load_dotenv()
 
 #### Load Env ####
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-DB_MINING = os.getenv('DB_MINING')
-DB_USERS = os.getenv('DB_USERS')
-CWD = os.getenv('CWD')
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+DB_MINING = os.getenv("DB_MINING")
+DB_USERS = os.getenv("DB_USERS")
+CWD = os.getenv("CWD")
 
-CHID_MINING = int(os.getenv('CHID_MINING')) # 採掘用ChID
-B_CHAT = int(os.getenv('B_CHAT')) #Brave雑談チャンネルID
-F_CHAT = int(os.getenv('F_CHAT')) #Freedom雑談チャンネルID
-G_CHAT = int(os.getenv('G_CHAT')) #Glory雑談チャンネルID
-P_CHAT = int(os.getenv('P_CHAT')) #Peaceful雑談チャンネルID
-B_ROLE = int(os.getenv('B_ROLE')) #Brave所属ロールID
-F_ROLE = int(os.getenv('F_ROLE')) #Freedom所属ロールID
-G_ROLE = int(os.getenv('G_ROLE')) #Glory所属ロールID
-P_ROLE = int(os.getenv('P_ROLE')) #Peaceful所属ロールID
-MCH = int(os.getenv('MCH'))
-B_EMOJI = os.getenv('B_EMOJI')
-F_EMOJI = os.getenv('F_EMOJI')
-G_EMOJI = os.getenv('G_EMOJI')
-P_EMOJI = os.getenv('P_EMOJI')
+CHID_MINING = int(os.getenv("CHID_MINING"))  # 採掘用ChID
+B_CHAT = int(os.getenv("B_CHAT"))  # Brave雑談チャンネルID
+F_CHAT = int(os.getenv("F_CHAT"))  # Freedom雑談チャンネルID
+G_CHAT = int(os.getenv("G_CHAT"))  # Glory雑談チャンネルID
+P_CHAT = int(os.getenv("P_CHAT"))  # Peaceful雑談チャンネルID
+B_ROLE = int(os.getenv("B_ROLE"))  # Brave所属ロールID
+F_ROLE = int(os.getenv("F_ROLE"))  # Freedom所属ロールID
+G_ROLE = int(os.getenv("G_ROLE"))  # Glory所属ロールID
+P_ROLE = int(os.getenv("P_ROLE"))  # Peaceful所属ロールID
+MCH = int(os.getenv("MCH"))
+B_EMOJI = os.getenv("B_EMOJI")
+F_EMOJI = os.getenv("F_EMOJI")
+G_EMOJI = os.getenv("G_EMOJI")
+P_EMOJI = os.getenv("P_EMOJI")
 
-DEBUG_CMD = os.getenv('DEBUG_CMD')
-RESET_CMD = os.getenv('RESET_CMD')
-MNG_CMD = os.getenv('MNG_CMD')
-ADD_CMD = os.getenv('ADD_CMD')
-MSG_CMD = os.getenv('MSG_CMD')
-START_CMD = os.getenv('START_CMD')
-STOP_CMD = os.getenv('STOP_CMD')
+DEBUG_CMD = os.getenv("DEBUG_CMD")
+RESET_CMD = os.getenv("RESET_CMD")
+MNG_CMD = os.getenv("MNG_CMD")
+ADD_CMD = os.getenv("ADD_CMD")
+MSG_CMD = os.getenv("MSG_CMD")
+START_CMD = os.getenv("START_CMD")
+STOP_CMD = os.getenv("STOP_CMD")
 ##################
 
 # mining flag
 MINE_OPEN = True
 
 # announce clock
-ANN_HOUR = [0,12]
+ANN_HOUR = [0, 12]
 ANN_MINUTE = [0]
 
 # mining probability
